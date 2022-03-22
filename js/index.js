db38a51f2dd34f26688497fd94e523f5fbf2e0d8
ramonfcf <ramonfcf@gmail.com>
2022-03-22 17:04:00 -0300

    solução temporária para adicionar nome dos personagens participantes em cada epi.

--- js/index.js
+++ js/index.js
@@ -10,9 +10,7 @@ botaoPersonagen.addEventListener('click', function(){
 botaoEpisodio.addEventListener('click', function(){
    let idEpisodio = document.querySelector('#input-id-episodio');
 
-   console.log(idEpisodio.value);
    let episodio = buscaEpisodio(idEpisodio.value);
-
 
 })
 
@@ -51,19 +49,41 @@ function buscaEpisodio(id) {
       divEpi.classList.add('principal__cartao-episodios');
 
       divEpi.innerHTML = `<p>Id: ${data.id}</p>
-                          <p>Nome: ${data.name}</p>
-                          <p>Estreia: ${data.air_date}</p>
-                          <p>Episódio: ${data.episode}</p>
-                          <p>Personagens: ${nomePersonagens}`
+      <p>Nome: ${data.name}</p>
+      <p>Estreia: ${data.air_date}</p>
+      <p>Episódio: ${data.episode}</p>`
 
-       let episodio = new Episodio (data.id, data.name, data.air_date, data.episode, data.characters);
+      let episodio = new Episodio (data.id, data.name, data.air_date, data.episode, data.characters);
+
+      let nomes = data.characters;
+      const nomesTeste = [];
+      
+      nomes.forEach(element => {
+        return nomesTeste.push(fetch(element).then(response => response.json()));
+      });
+   
+      Promise.all(nomesTeste)
+         .then(personagens => {
+            let texto = '';
+   
+            personagens.forEach(element => {
+               texto += `${element.name}; `;
+            })
+
+         divEpi.innerHTML = `<p><span class="principal__episodio-descricao">Id:</span> ${data.id}</p>
+         <p><span class="principal__episodio-descricao">Nome:</span> ${data.name}</p>
+         <p><span class="principal__episodio-descricao">Estreia:</span> ${data.air_date}</p>
+         <p><span class="principal__episodio-descricao">Episódio:</span> ${data.episode}</p>
+         <p><span class="principal__episodio-descricao">Personagens:</span> ${texto}`
+   
+         })
+
          return episodio;
-
 
    })
 }
 
-
+/*
 
 async function buscaNome(id){
    let response = await fetch(`https://rickandmortyapi.com/api/episode/${id}`);
@@ -71,14 +91,27 @@ async function buscaNome(id){
 
    let nomes = userdata.characters;
 
-   let nomePersonagensEpisodio = [];
+   let nomePersonagensEpisodio = '';
 
-   await nomes.forEach(async element => {
-      let respPersonagem =  await fetch(element);
-      let dadosPersonagem = await respPersonagem.json();
+   const nomesTeste = [];
 
-      nomePersonagensEpisodio.push(dadosPersonagem.name)
+   await nomes.forEach(element => {
+      nomesTeste.push(fetch(element).then(response => response.json()));
    });
 
-   console.log(nomePersonagensEpisodio);
+
+   Promise.all(nomesTeste)
+      .then(personagens => {
+         let texto = '';
+
+         personagens.forEach(element => {
+            texto += `${element.name}, `;
+
+         })
+
+         nomePersonagensEpisodio = texto;
+      })
+      console.log(nomePersonagensEpisodio)
+      return nomePersonagensEpisodio;
 }
+*/
