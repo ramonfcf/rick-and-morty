3ab2ff3f677e1da50af5eaa49a65ffeca8f7a28b
ramonfcf <ramonfcf@gmail.com>
2022-04-01 12:26:59 -0300

    atualizando readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # rick-and-morty
+
+## Projeto Pessoal
+
+#### O projeto foi construido com base na Api - https://rickandmortyapi.com/ de Rick and Morty. Ela busca dados sobre nome, id, fotos, se o personagem ainda vive, sua origem, etc, dos episódios da serie animada de Rick and Morty.
+
+###### O projeto foi construido de forma a fixar a utilização do fech do JavaScript, funções async/await.
+
+</br>
+
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white) ![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white) ![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+
+</br>
+
+
+![Imagem do Site](https://images2.imgbox.com/d3/e3/d5CUMpDi_o.jpg)
+
--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 let botaoPersonagen = document.querySelector('#botao-personagem');
 let botaoEpisodio = document.querySelector('#botao-episodio');
 
-botaoPersonagen.addEventListener('click', function(){
-  let idPersonagem = document.querySelector('#input-id-personagem');
+botaoPersonagen.addEventListener('click', function () {
+   let idPersonagem = document.querySelector('#input-id-personagem');
 
    let personagem = buscaPersonagem(idPersonagem.value);
-   
+
    idPersonagem.focus();
 })
-botaoEpisodio.addEventListener('click', function(){
+botaoEpisodio.addEventListener('click', function () {
    let idEpisodio = document.querySelector('#input-id-episodio');
 
    let episodio = buscaEpisodio(idEpisodio.value);
@@ -16,59 +16,56 @@ botaoEpisodio.addEventListener('click', function(){
    idEpisodio.focus();
 })
 
-
 function buscaPersonagem(id) {
-    
+
    return fetch(`https://rickandmortyapi.com/api/character/${id}`)
-   .then(res => res.json())
-   .then(data => {
+      .then(res => res.json())
+      .then(data => {
 
-      let tbody = document.querySelector('.tabela__personagem-tbody');
-      let divPerso = document.querySelector('.principal__personagens');
-      divPerso.classList.add('principal__cartao-personagens');
+         let tbody = document.querySelector('.tabela__personagem-tbody');
+         let divPerso = document.querySelector('.principal__personagens');
+         divPerso.classList.add('principal__cartao-personagens');
 
-      divPerso.innerHTML = `<img class="principal__personagem-imagem" src="https://rickandmortyapi.com/api/character/avatar/${data.id}.jpeg">
+         divPerso.innerHTML = `<img class="principal__personagem-imagem" src="https://rickandmortyapi.com/api/character/avatar/${data.id}.jpeg">
                            <p><span class="principal__episodio-descricao">Id:</span> ${data.id}</p>
                            <p><span class="principal__episodio-descricao">Nome:</span> ${data.name}</p>
                            <p><span class="principal__episodio-descricao">Estado:</span> ${data.status}</p>
                            <p><span class="principal__episodio-descricao">Gênero:</span> ${data.gender}</p>
                            <p><span class="principal__episodio-descricao">Origem:</span> ${data.origin['name']}`
 
-       let personagem = new Personagem (data.id, data.name, data.status, data.gender, data.origin['name']);
-         console.log(personagem)  
+         let personagem = new Personagem(data.id, data.name, data.status, data.gender, data.origin['name']);
+         console.log(personagem)
          return personagem;
-   })
+      })
 }
-
 
 
 function buscaEpisodio(id) {
    return fetch(`https://rickandmortyapi.com/api/episode/${id}`)
-   .then(res => res.json())
-   .then(data => {
+      .then(res => res.json())
+      .then(data => {
 
-      let divEpi = document.querySelector('.principal__episodios');
-      divEpi.classList.add('principal__cartao-episodios');
+         let divEpi = document.querySelector('.principal__episodios');
+         divEpi.classList.add('principal__cartao-episodios');
 
-      let episodio = new Episodio (data.id, data.name, data.air_date, data.episode, data.characters);
+         let episodio = new Episodio(data.id, data.name, data.air_date, data.episode, data.characters);
 
-      let nomesPersonagens = buscaNome(id).then(response => {divEpi.innerHTML = `<p><span class="principal__episodio-descricao">Id:</span> ${data.id}</p>
+         let nomesPersonagens = buscaNome(id).then(response => {
+            divEpi.innerHTML = `<p><span class="principal__episodio-descricao">Id:</span> ${data.id}</p>
       <p><span class="principal__episodio-descricao">Nome:</span> ${data.name}</p>
       <p><span class="principal__episodio-descricao">Estreia:</span> ${data.air_date}</p>
       <p><span class="principal__episodio-descricao">Episódio:</span> ${data.episode}</p>
       <p><span class="principal__episodio-descricao">Personagens:</span> ${response}`
 
-   }
-)
+         }
+         )
 
          return episodio;
 
-   })
+      })
 }
 
-
-
-async function buscaNome(id){
+async function buscaNome(id) {
    let response = await fetch(`https://rickandmortyapi.com/api/episode/${id}`);
    let userdata = await response.json();
 
